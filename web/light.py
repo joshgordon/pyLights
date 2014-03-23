c843cbad63473527273518122f61d64579b65b58
Josh Gordon <github@joshgordon.net>
2014-03-22 23:30:55 -0400

    Working on getting stuff in shape to get it ready to bottle.

--- web/light.py
+++ web/light.py
@@ -9,9 +9,38 @@
 import serial
 import color
 from time import sleep
+from __future__ import print_function
+import sys
 
 #set up the serial port. 
 ser = serial.Serial('/dev/tty.usbmodemfd1221', 9600, timeout=1)
+
+class ColorLight: 
+    def __init__(lightSet): 
+        valid=['a', 'b'] # Valid Light sets. 
+        self.ser = ser
+        if lightSet in valid: 
+            color = getColorTuple(lightSet)
+            self.r = color[0]
+            self.g = color[1] 
+            self.b = color[2] 
+            self.lightSet = lightSet
+        else: 
+            raise Exception("Invalid Light") 
+        
+    def setColor(self, r, g, b): 
+        print("Setting color {0} {1} {2}".format(r, g, b), file=sys.stderr) 
+        self.r = r
+        self.g = g
+        self.b = b 
+        self.ser.write(self.lightSet + chr(self.r) + chr(self.g) + chr(self.b))
+        
+
+    def setColorByName(self, colorName): 
+        colors[colorName].getColor()
+        
+
+
 
 def getColor(set): 
     ser.write(set.upper()) 
@@ -36,6 +65,8 @@ colors['11pm'] = color.Color(ser, 75, 25, 1)
 colors['midnight'] = color.Color(ser, 50, 10, 0)
 colors['1am'] = color.Color(ser, 25, 5, 0)
 colors['dimWhite'] = color.Color(ser, 50, 10, 3)
+
+def setA(r, g, b)
 
 #print "setting red" 
 # colors['red'].set(ser)
---
+++ web/web.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python2 
+from bottle import route, request, run, get, template
+
+import light 
+
