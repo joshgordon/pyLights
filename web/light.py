124a8f6f965977b831f27fe84f4e52237d2f4b73
Josh Gordon <github@joshgordon.net>
2014-03-26 20:36:24 -0400

    Working on a few things.

--- web/light.py
+++ web/light.py
@@ -14,7 +14,7 @@ import sys
 import time 
 
 #set up the serial port. 
-ser = serial.Serial('/dev/tty.usbmodemfd1221', 9600, timeout=1)
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 
 class ColorLight: 
     def __init__(lightSet): 
@@ -73,36 +73,3 @@ class ColorLight:
     def getColor(self): 
         return (self.r, self.g, self.b) 
 
-
-
-def getColor(set): 
-    ser.write(set.upper()) 
-    ##This is ugly, but I really don't feel like reflashing the arduino
-    # to turn it into something prettier... 
-    return list(str(ser.readline()).strip().split(','))
-    # return allCols
-    # return list([75, 83, 23] )
-        
-def getColorTuple(set): 
-    color = getColor(set)
-    return map(int, color)
-    
-# colors = dict()
-
-# colors['red'] = color.Color(ser, 255, 0, 0) 
-# colors['green'] = color.Color(ser, 0, 255, 0)
-# colors['blue'] = color.Color(ser, 0, 0, 255)
-# colors['white'] = color.Color(ser, 255, 160, 60) 
-# colors['black'] = color.Color(ser, 0, 0, 0)
-# colors['11pm'] = color.Color(ser, 75, 25, 1)
-# colors['midnight'] = color.Color(ser, 50, 10, 0)
-# colors['1am'] = color.Color(ser, 25, 5, 0)
-# colors['dimWhite'] = color.Color(ser, 50, 10, 3)
-
-def setA(r, g, b)
-
-#print "setting red" 
-# colors['red'].set(ser)
-# sleep(5)
-# print "setting white" 
-# colors['white'].set(ser)
