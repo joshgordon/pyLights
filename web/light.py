9c80dcb1e3b6dc1004fab78e4b32a88973c798d5
ohnoitsyou <ohnoitsyou7043@gmail.com>
2014-08-31 16:25:31 -0400

    wired up sliders
    Adjusted the table and fixed the submit code

--- web/light.py
+++ web/light.py
@@ -1,128 +1,128 @@
-# Uses the color class and a dictionary to have a set of colors. More will be 
-# here soon. 
-# Written by Josh Gordon (github.com/joshgordon) 
-# All code is licensed under the GNU GPL, Version 2 or later. 
+# Uses the color class and a dictionary to have a set of colors. More will be
+# here soon.
+# Written by Josh Gordon (github.com/joshgordon)
+# All code is licensed under the GNU GPL, Version 2 or later.
 
 
-# Sorry for the lack of comments, I kinda threw this together quickly... 
+# Sorry for the lack of comments, I kinda threw this together quickly...
 
 from __future__ import print_function
-import serial
+#import serial
 # import colors
 from time import sleep
 import sys
-import time 
+import time
 import threading
 
-#set up the serial port. 
+#set up the serial port.
 # ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 
 
 
-class ColorLight: 
-    def __init__(self, lightSet): 
-        valid=['a', 'b'] # Valid Light sets. 
+class ColorLight:
+    def __init__(self, lightSet):
+        valid=['a', 'b'] # Valid Light sets.
         #self.ser = ser
         self.colors=3
-        if lightSet in valid: 
+        if lightSet in valid:
             # color = getColorTuple(lightSet)
             self.r = 0
             self.g = 0
             self.b = 0
             self.lightSet = lightSet
-        else: 
-            raise Exception("Invalid Light") 
-        
-    def setColor(self, color): 
-        print("Setting color %i %i %i" % color, file=sys.stderr) 
-        self.r = color[0] 
+        else:
+            raise Exception("Invalid Light")
+
+    def setColor(self, color):
+        print("Setting color %i %i %i" % color, file=sys.stderr)
+        self.r = color[0]
         self.g = color[1]
-        self.b = color[2] 
+        self.b = color[2]
         #self.ser.write(self.lightSet + chr(self.r) + chr(self.g) + chr(self.b))
-        
+
 
     ## The main point of this is to keep overhead down when fading. 
-    def setDirect(self, color): 
-        print("Setting color %i %i %i" % color, file=sys.stderr) 
+    def setDirect(self, color):
+        print("Setting color %i %i %i" % color, file=sys.stderr)
         #self.ser.write(self.lightSet + chr(color[0]) + chr(color[1]) + chr(color[2]))
-        
-#    def setColorByName(self, colorName): 
+
+#    def setColorByName(self, colorName):
 #        r, g, b = color.getColor(colorName)
 #        self.setColor(r, g, b)
 
-    def fade(self, fTime, end): 
-        start = self.getColor() 
-        self.r = end[0] 
-        self.g = end[1] 
-        self.b = end[2] 
-        print("fading color %i %i %i" % end, file=sys.stderr) 
-        #threading.Thread(target=self.fadeReal, args=(fTime, start, end)).start() 
+    def fade(self, fTime, end):
+        start = self.getColor()
+        self.r = end[0]
+        self.g = end[1]
+        self.b = end[2]
+        print("fading color %i %i %i" % end, file=sys.stderr)
+        #threading.Thread(target=self.fadeReal, args=(fTime, start, end)).start()
 
     def fadeReal(self, fTime, start, end):
         exp= 4/3.0
-        for i in range(65): 
-            current = list() 
-            for c in range(self.colors): 
-                if end[c] > start[c]: 
+        for i in range(65):
+            current = list()
+            for c in range(self.colors):
+                if end[c] > start[c]:
                     current.append(int((end[c]-start[c])/256.0 * i ** exp + start[c]))
-                else: 
+                else:
                     current.append(int(start[c] - (start[c]-end[c])/255.0 * i ** exp))
-                    
+
             self.setDirect((current[0], current[1], current[2]))
             time.sleep(fTime/64.0)
 
-        # make sure we update the variables. 
+        # make sure we update the variables.
         self.setColor(end)
 
 
 #TODO
-#    def fadeByName(self, fTime, colorName): 
-#        thisColor = color.getColor(colorName) 
-#        self.fade(fTime, thisColor) 
-                
-            
-    def getColor(self): 
-        return (self.r, self.g, self.b) 
+#    def fadeByName(self, fTime, colorName):
+#        thisColor = color.getColor(colorName)
+#        self.fade(fTime, thisColor)
 
 
-class MonoLight: 
-    def __init__(self, lightSet): 
-        valid=['w'] # valid Light sets. 
-        # self.ser = ser 
-        self.colors=1 
-        if lightSet in valid: 
-            #color=getColorTuple(lightSet) 
+    def getColor(self):
+        return (self.r, self.g, self.b)
+
+
+class MonoLight:
+    def __init__(self, lightSet):
+        valid=['w'] # valid Light sets.
+        # self.ser = ser
+        self.colors=1
+        if lightSet in valid:
+            #color=getColorTuple(lightSet)
             self.b = 0
             self.lightSet = lightSet
-        else: 
-            raise Exception("Invalid Light") 
-            
-            
-    def setColor(self, color): 
-        self.b = color[0]
-        print("Setting color %i" % color, file=sys.stderr) 
-        # self.ser.write(self.lightSet + chr(self.b))
-        
+        else:
+            raise Exception("Invalid Light")
 
-    def setDirect(self, color): 
-        print("Setting color %i" % color, file=sys.stderr) 
+
+    def setColor(self, color):
+        self.b = color[0]
+        print("Setting color %i" % color, file=sys.stderr)
+        # self.ser.write(self.lightSet + chr(self.b))
+
+
+    def setDirect(self, color):
+        print("Setting color %i" % color, file=sys.stderr)
         # self.ser.write(self.lightSet + chr(color[0]))
 
-    def fade(self, fTime, end): 
+    def fade(self, fTime, end):
         start = self.getColor()[0]
         self.b = end[0]
-        print("Setting color %i" % end, file=sys.stderr) 
-        # threading.Thread(target=self.fadeReal, args=(fTime, start, end)).start() 
+        print("Setting color %i" % end, file=sys.stderr)
+        # threading.Thread(target=self.fadeReal, args=(fTime, start, end)).start()
 
     def fadeReal(self, fTime, start, end):
         exp=4/3.0
         end = end[0]
-        for i in range(65): 
-            if end > start: 
+        for i in range(65):
+            if end > start:
                 current=(int((end-start)/256.0 * i ** exp + start))
-            else: 
+            else:
                 current=(int(start - (start-end)/255.0 * i ** exp))
-                    
+
             self.setDirect((current, ))
             time.sleep(fTime/64.0)
 
@@ -140,7 +140,7 @@ class MonoLight:
 #     return list(str(ser.readline()).strip().split(','))
 #     # return allCols
 #     # return list([75, 83, 23] )
-        
+
 # def getColorTuple(set):
 #     color = getColor(set)
 #     return map(int, color)
